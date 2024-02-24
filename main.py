12f4a39a62af023bd3b296ace122fe1a44a7f65c
Carl Justine Malabanan <carljustinem984@gmail.com>
2024-02-24 20:50:07 +0800

    Updated Main.py

--- main.py
+++ main.py
@@ -1,47 +1,96 @@
-class operations:
+import os
+class Item:
     output = ''
-    def create(self,id,name,description,price):
-        f = open('inventory.txt', 'a')
-        self.output += f'{id}, {name}, {description}, {price}\n'
-        f.write(self.output)
-        f.close()
-    def read(self):
-        f = open('inventory.txt', 'r')
-        records = f.read()
-        print(records)
-        f.close()
-    def update(self):
-        id = input('ID to edit: ')
-        name = input('Name to edit: ')
-        description = input('Description to edit: ')
-        price = input('Price to edit: ')
-        
-        f = open('inventory.txt', 'r')
-        records = f.readlines()
-        f.close()
-        
-        f = open('inventory.txt', 'w')
-        for record in records:
-            if record.split(',')[0] == id:
-                f.write(f'{id}, {name}, {description}, {price}')
-            else: 
-                f.write(record)
-        f.close()
-        
-    def delete(self):
-        f = open('inventory.txt', 'a')
-        f.write('')
-        
-operate = operations()
+    def addItem(self,itemID,itemName,itemDescription,itemPrice):
+        try:
+            f = open('inventory.txt', 'a')
+            self.output = f'{itemID}, {itemName}, {itemDescription}, {itemPrice}\n'
+            f.write(self.output)
+            f.close()
+            print('Successfully Added Item.')
+        except:
+            print('Error Occured while writing in inventory.txt.')
 
-print("Welcome to the inventory management system")
-print("1. Create\n2. Read\n3. Update\n3. Delete")
-choice = int(input('Choice: '))
-if choice == 1:
-    operate.create(1,'Banana','Fruit','$1.00')
-elif choice == 2:
-    operate.read()
-elif choice == 3:
-    operate.update()
-elif choice == 4:
-    operate.delete()
+    def readItems(self):
+        try:
+            f = open('inventory.txt', 'r')
+            records = f.read()
+            print("Showing All Items in inventory.txt: \n")
+            print(records)
+        except:
+            print('Error Occured while tring to read inventory.txt.')
+        
+    def updateItem(self,itemID,itemName,itemDescription,itemPrice):
+        try:
+            f = open('inventory.txt', 'r')
+            Items = f.readlines()
+            f.close()
+            
+            f = open('inventory.txt', 'w')
+            for item in Items:
+                if item.split(',')[0] == itemID:
+                    self.output = f'{itemID}, {itemName}, {itemDescription}, {itemPrice}\n'
+                    f.write(self.output)
+                else: 
+                    f.write(item)
+            f.close()
+            print('Successfully Updated Item.')
+        except: 
+            print('Error Occured while updating record.')
+    def deleteItem(self,itemID):
+        try:
+            f = open('inventory.txt','r')
+            Items = f.readlines()
+            f.close()
+            
+            f = open('inventory.txt','w')
+            for item in Items:
+                if item.split(',')[0] != itemID:
+                    f.write(item)
+                else: continue
+            f.close()
+            print('Successfully Deleted Item.')
+        except: 
+            print("Error Occured while deleting a record.")
+            
+def inputItemInfo():
+    itemID = input('Enter item ID: ')
+    itemName = input('Enter item name: ')
+    itemDescription = input('Enter item description: ')
+    itemPrice = input('Enter item price: ')
+    return itemID, itemName, itemDescription, itemPrice
+
+newItem = Item()
+Notfinished = True
+while Notfinished == True:
+    try:
+        print("Welcome to the inventory management system")
+        print("1. Create\n2. Read\n3. Update\n4. Delete")
+        choice = int(input('Choice: '))
+    except ValueError:
+        print('Please enter a number')
+        os.system('pause')
+        os.system('cls')
+        continue
+
+    if choice == 1:
+        newItem.addItem(*inputItemInfo())
+    elif choice == 2:
+        newItem.readItems()
+    elif choice == 3:
+        newItem.updateItem(*inputItemInfo())
+    elif choice == 4:
+        itemID = input('Enter Item ID to delete: ')
+        newItem.deleteItem(itemID)
+    
+
+    redo = input('Are you finished? (y/n): ')
+    redo = redo.upper()
+    
+    if redo == 'Y':
+        Notfinished = False
+        break
+    
+    os.system('cls')
+    
+    
